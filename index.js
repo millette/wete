62102e71fef33cbbe7138a925f867e4f7d86e9c1
Robin Millette <robin@millette.info>
2020-02-17 15:02:19 -0500

    Transform markdown to html.

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict"
+
+// npm
+const vfile = require("to-vfile")
+const visit = require("unist-util-visit")
+const unified = require("unified")
+const markdown = require("remark-parse")
+const remark2rehype = require("remark-rehype")
+const html = require("rehype-stringify")
+const remarkAttr = require("remark-attr")
+const sanitize = require("rehype-sanitize")
+const merge = require("deepmerge")
+const gh = require("hast-util-sanitize/lib/github")
+
+const move = (options) => {
+  const expected = (options || {}).extname
+  if (!expected) throw new Error("Missing `extname` in options")
+
+  return (tree, file) => {
+    if (file.extname !== expected) file.extname = expected
+  }
+}
+
+const embedder = () => (tree, file) => {
+  if (!file.contents) throw new Error("empty")
+  visit(tree, (node) => {
+    if (
+      node.type === "link" &&
+      node.data &&
+      node.data.hProperties &&
+      node.data.hProperties["data-embed"] === ""
+    )
+      node.data.hProperties["data-embed"] = true
+  })
+}
+
+const trFile = (() => {
+  const a = ["data-embed"]
+  const mdOpts = { footnotes: true, gfm: true, commonmark: true }
+  const process = unified()
+    .use(markdown, mdOpts)
+    .use(remarkAttr, { scope: "extended", extend: { link: a } })
+    .use(move, { extname: ".html" })
+    .use(embedder)
+    .use(remark2rehype, mdOpts)
+    .use(html, mdOpts)
+    .use(sanitize, merge(gh, { attributes: { a } })).process
+
+  return async (fn) => {
+    const a = typeof fn === "string" ? await vfile.read(fn) : new vfile(fn)
+    const b = await process(a)
+    await vfile.write(b)
+    return b
+  }
+})()
+
+module.exports = trFile
