04cfe0a78f9ce866ea798c531352b9190dccc49b
Robin Millette <robin@millette.info>
2020-02-21 19:29:23 -0500

    Login/logout; view/edit/cancel.

--- main.js
+++ main.js
@@ -1,9 +1,234 @@
 // npm
 // import { exec, init } from "pell/src/pell"
-import { neverland as $, render, html, useState } from "neverland"
+import { neverland as $, render, html, useState, useEffect } from "neverland"
 // import { html as h2 } from 'lighterhtml'
+// import { render, html } from "lighterhtml"
+import Cookies from "js-cookie"
 
-const $isConnected = document.getElementById("is-connected")
+// getCookie
+// const verifyCookie = (name) => {
+const getCookie = (name) => {
+  // Cookies.get(`${name}-signed`)
+  // check signature using public key
+  // if bad, remove it
+  // Cookies.remove(name)
+  // Cookies.remove(`${name}-signed`)
+  return Cookies.get(name)
+}
+
+/*
+const showActions = () => {
+  const c = html`
+    <div class="tabs is-boxed is-right is-large">
+      <ul>
+        <li class="is-active"><a>View</a></li>
+        <li><a>Edit</a></li>
+      </ul>
+    </div>`
+
+  const $cnt = document.getElementById("cnt")
+  const $it = $cnt.parentNode.insertBefore(document.createElement("div"), $cnt)
+
+  render($it, c)
+}
+*/
+
+const Actions = $((un) => {
+  const [aa, bb] = useState()
+  console.log("PING-Actions", aa)
+
+  useEffect(() => {
+    console.log("EFFECT", aa)
+    document.getElementById("cnt").style = aa
+      ? "display: none"
+      : "display: block"
+  }, [aa])
+
+  const clickEdit = (ev) => {
+    if (aa) return
+    ev.preventDefault()
+    console.log("clickEdit")
+    bb(true)
+  }
+
+  const clickView = (ev) => {
+    if (!aa) return
+    ev.preventDefault()
+    console.log("clickView")
+    bb(false)
+  }
+
+  const c = html`
+    <div
+      id="app-actions"
+      class="tabs is-toggle is-toggle-rounded is-right is-large"
+    >
+      <ul>
+        <li class=${aa ? "" : "is-active"}>
+          <a onclick=${clickView} href="#">${aa ? "Cancel" : "View"}</a>
+        </li>
+        <li class=${aa ? "is-active" : ""}>
+          <a href="#" onclick=${clickEdit}
+            >Edit&nbsp;<small>(as ${un})</small></a
+          >
+        </li>
+      </ul>
+    </div>
+  `
+
+  return c
+})
+
+const showActions = (un) => {
+  // const $p = document.getElementById("cnt").parentNode
+  // const $fc = $p.firstChild
+
+  /*
+  let editMode = false
+
+  const clickEdit = (ev) => {
+    ev.preventDefault()
+    console.log("clickEdit")
+    editMode = true
+  }
+
+  const clickView = (ev) => {
+    ev.preventDefault()
+    console.log("clickView")
+    editMode = false
+  }
+
+  const c = html`
+    <div id="app-actions" class="tabs is-boxed is-right is-large">
+      <ul>
+        <li class=${editMode ? "" : "is-active"}><a onclick=${clickView} href="#">View</a></li>
+        <li class=${editMode ? "is-active" : ""}><a href="#" onclick=${clickEdit}>Edit</a></li>
+      </ul>
+    </div>`
+  */
+
+  const c = Actions(un)
+
+  const $cnt = document.getElementById("cnt")
+  const $x = $cnt.parentNode.insertBefore(document.createElement("div"), $cnt)
+
+  render($x, c)
+}
+
+const hideActions = () => {
+  const $p = document.getElementById("app-actions")
+  // console.log("P", $p)
+  // console.log("PP", $p && $p.parentNode)
+  if ($p) $p.parentNode.removeChild($p)
+
+  /*
+  const $p = document.getElementById("cnt").parentNode
+  const $fc = $p.firstChild
+  console.log("P", $p)
+  console.log("FC", $fc, $fc.tagName)
+  if ($fc.tagName === "DIV") $p.removeChild($p.firstChild)
+  */
+}
+
+const Connected = () => {
+  const username = getCookie("connected")
+
+  const con = () => {
+    Cookies.set("connected", "bob")
+    Connected()
+  }
+
+  const discon = () => {
+    Cookies.remove("connected")
+    Connected()
+  }
+
+  const yup = html`
+    <button onclick=${discon} class="button is-warning">
+      Logout (${username})
+    </button>
+  `
+
+  const nope = html`
+    <div id="is-connected" class="buttons">
+      <button class="button is-primary">
+        <strong>Sign up</strong>
+      </button>
+      <button class="button is-light" onclick=${con}>
+        Log in
+      </button>
+    </div>
+  `
+
+  if (username) {
+    render(document.getElementById("is-connected"), yup)
+    showActions(username)
+  } else {
+    render(document.getElementById("is-connected"), nope)
+    hideActions()
+  }
+}
+
+// Connected($isConnected, Cookies.get("connected"))
+Connected()
+
+/*
+const Connected = $((connected) => {
+  // const connected = Cookies.get('connected')
+  const [connected666, setConnected666] = useState()
+  console.log("PING", connected)
+  // console.log("PING", initialState, connected)
+  // const orig = html.for(orig2.innerHTML)
+
+  const discon = (ev) => {
+    ev.preventDefault()
+    console.log("discon", connected, connected666)
+    // setConnected("")
+    Cookies.remove("connected")
+    setConnected666()
+    setTimeout(() => console.log("discon2", connected, connected666), 500)
+  }
+
+
+
+  const con = (ev) => {
+    ev.preventDefault()
+    console.log("con", connected, connected666)
+    Cookies.set("connected", "bob")
+    setConnected666("bob")
+    setTimeout(() => console.log("con2", connected, connected666), 500)
+    // setConnected("")
+  }
+
+
+  // if (!connected) return html`<p>Thing</p>`
+  if (!connected666)
+    return html`
+      <div id="is-connected" class="buttons">
+        <button class="button is-primary">
+          <strong>Sign up</strong>
+        </button>
+        <button class="button is-light" onclick=${con}>
+          Log in
+        </button>
+      </div>
+    `
+
+  return html`
+    <button onclick=${discon} class="button is-warning">
+      Logout (${connected} --- ${connected666})
+    </button>
+  `
+})
+
+const x = render($isConnected, Connected(Cookies.get('connected')))
+
+console.log("XXX1")
+console.log(x)
+console.log("XXX2", typeof x)
+console.log("XXX3", Object.keys(x))
+*/
+
 // const orig2 = $isConnected.cloneNode()
 
 /*
@@ -18,7 +243,7 @@ console.log("XXX2", typeof x)
 console.log("XXX3", Object.keys(x))
 */
 
-todo($isConnected)
+// todo($isConnected)
 // todo(document.body.lastChild)
 
 function todo(node, items = []) {
@@ -45,6 +270,7 @@ function todo(node, items = []) {
   }
 }
 
+/*
 const Connected = $(() => {
   const [connected, setConnected] = useState("")
   console.log("PING", connected)
@@ -76,6 +302,7 @@ const Connected = $(() => {
     </button>
   `
 })
+*/
 
 // const zaza = html`<div>There ${Connected("robin")}</div>`
 // render($isConnected, zaza) // "robin"
