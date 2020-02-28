9c0e09171f64477fdafd481f17c10c433723f87e
Robin Millette <robin@millette.info>
2020-02-28 15:45:14 -0500

    Extract all links, local and external.

--- backlinks.js
+++ backlinks.js
@@ -2,21 +2,37 @@
 const fs = require("fs").promises
 
 // npm
+const level = require("level")
 const unified = require("unified")
 const parse = require("rehype-parse")
-// const sanitize = require("rehype-sanitize")
+const sanitize = require("rehype-sanitize")
 const stringify = require("rehype-stringify")
 const { selectAll } = require("hast-util-select")
 
+const baseUrl = new URL("http://localhost/")
+
+const db = level("db", {
+  valueEncoding: "json",
+})
+
 const yaya = () => (tree, file) => {
-  console.log(tree && Object.keys(tree), file && Object.keys(file))
-  const x = selectAll("a", tree)
-  console.log("X", JSON.stringify(x, null, 2))
+  console.log(Object.keys(file))
+  const x = selectAll("a", tree).map(
+    ({ properties, children: [{ value }] }) => ({
+      properties: {
+        ...properties,
+        href: new URL(properties.href, baseUrl),
+      },
+
+      value,
+    })
+  )
+  console.log(JSON.stringify(x, null, 2))
 }
 
 const process2 = unified()
   .use(parse)
-  // .use(sanitize)
+  .use(sanitize)
   .use(yaya)
   .use(stringify).process
 
@@ -24,6 +40,9 @@ const run = async () => {
   // const cnt = await fs.readFile("written/wiki.html", "utf-8")
   const cnt = await fs.readFile("written/ikiw.html", "utf-8")
   console.log(cnt.length)
+
+  // const oy = await db.get(["page", "ikiw"].join(":"))
+  // console.log(Object.keys(oy))
 
   const p2 = await process2(cnt)
   // console.log(p2)
