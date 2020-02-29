b6d4d3c550775f8f27f1669ec856b0e757c28059
Robin Millette <robin@millette.info>
2020-02-29 13:56:51 -0500

    Store recent changes; other touchups.

--- backlinks.js
+++ backlinks.js
@@ -1,8 +1,5 @@
 "use strict"
 
-// core
-// const fs = require("fs").promises
-
 // npm
 const level = require("level")
 const reporter = require("vfile-reporter")
@@ -16,8 +13,6 @@ const db = level("db", {
 })
 
 const run = async (pathname) => {
-  // const contents = await fs.readFile(["written", pathname, ".html"].join(""), "utf-8")
-
   const allPages0 = await getPrefixedKeys(db, "page")
   const allPages = allPages0.map((id) => `/${id}`)
 
@@ -26,34 +21,21 @@ const run = async (pathname) => {
   const { cnt: contents, ...oy } = await db.get(
     ["page", pathname.slice(1)].join(":")
   )
-  // const oy = { internalLinks: [], externalLinks: [] }
   console.log("original size", contents.length, oy)
 
   console.log(oy)
   if (!oy.internalLinks) oy.internalLinks = []
   if (!oy.externalLinks) oy.externalLinks = []
 
-  /*
-  oy.internalLinks.push({
-    href: "/wiki#tada",
-    page: "/wiki",
-    text: "rah rah"
-  })
-  */
-
   const processor = makeProcessor(allPages)
   const vfout = await processor({ pathname, contents })
 
   console.log(reporter(vfout))
-  // console.log(vfout)
   console.log("output size", vfout.contents.length)
-  // console.log(JSON.stringify(vfout.data.yaya, null, 2))
-  // console.log(vfout.data.yaya.externalLinks[0].properties.href)
 
   const deletedBacklinks = []
 
   oy.internalLinks.forEach((obj) => {
-    // console.log("DELETED?", obj)
     if (
       vfout.data.yaya.internalLinks
         .filter(({ missing }) => !missing)
@@ -69,7 +51,6 @@ const run = async (pathname) => {
   const newBacklinks = []
 
   vfout.data.yaya.internalLinks.forEach(({ missing, ...obj }) => {
-    // console.log("NEW???", missing, obj)
     newLinks.push(obj)
     if (
       !missing &&
@@ -79,9 +60,6 @@ const run = async (pathname) => {
         ["backlink", obj.page.slice(1), pathname.slice(1)].join(":")
       )
   })
-  // vfout.data.yaya.internalLinks = newLinks
-  // console.log('vfout', JSON.stringify(vfout, null, 2))
-  // console.log('newBacklinks', newBacklinks)
 
   const date = Date.now()
   const version = ["page-version", pathname.slice(1), date].join(":")
@@ -90,27 +68,26 @@ const run = async (pathname) => {
     cnt: vfout.contents,
     internalLinks: newLinks,
     externalLinks: vfout.data.yaya.externalLinks,
-    date,
-    // connected: oy.connected
   }
+
+  const type = "put"
 
   const batch = newBacklinks.map((key) => ({
     key,
-    value: true,
-    type: "put",
+    value: date,
+    type,
   }))
 
-  deletedBacklinks.forEach((x) => {
+  deletedBacklinks.forEach((key) => {
     const zo = {
-      key: x,
+      key,
       type: "del",
     }
-    // console.log("ZO", zo)
     batch.push(zo)
   })
 
   batch.push({
-    type: "put",
+    type,
     key: ["page", pathname.slice(1)].join(":"),
     value: {
       ...newDoc,
@@ -119,7 +96,13 @@ const run = async (pathname) => {
   })
 
   batch.push({
-    type: "put",
+    type,
+    key: ["change", date, pathname.slice(1)].join(":"),
+    value: true,
+  })
+
+  batch.push({
+    type,
     key: version,
     value: newDoc,
   })
@@ -128,6 +111,6 @@ const run = async (pathname) => {
   return db.batch(batch)
 }
 
-// run("/ikiw").catch(console.error)
+run("/ikiw").catch(console.error)
 // run("/wiki").catch(console.error)
-run("/todo").catch(console.error)
+// run("/todo").catch(console.error)
