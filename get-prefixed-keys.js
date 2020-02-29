7e8f0bce415b145fa9a52a3ccc3216a1df147212
Robin Millette <robin@millette.info>
2020-02-29 02:21:10 -0500

    Support fetch all keys.

--- get-prefixed-keys.js
+++ get-prefixed-keys.js
@@ -3,14 +3,15 @@
 // npm
 const { toArray } = require("streamtoarray")
 
-const getPrefixedKeys = async (elDb, prefix = "") => {
+const getPrefixedKeys = async (elDb, prefix = 0) => {
   if (!elDb) throw new Error("Missing args.")
-  const len = prefix.length + 1
-  const slicer = (id) => `/${id.slice(len)}`
-  const str = await elDb.createKeyStream({
-    gt: [prefix, ""].join(":"),
-    lt: [prefix, "\ufff0"].join(":"),
-  })
+
+  const gt = prefix ? [prefix, ""].join(":") : "_"
+  const lt = prefix ? [prefix, "\ufff0"].join(":") : "\ufff0"
+
+  const len = prefix && prefix.length + 1
+  const slicer = (id) => id.slice(len)
+  const str = await elDb.createKeyStream({ gt, lt })
   const arr = await toArray(str)
   return arr.map(slicer)
 }
