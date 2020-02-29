ec3ef4384a345d6433341ed8b474cb7f06b9e5d2
Robin Millette <robin@millette.info>
2020-02-29 02:26:29 -0500

    Skip keys starting with '_'.

--- get-prefixed-keys.js
+++ get-prefixed-keys.js
@@ -6,7 +6,7 @@ const { toArray } = require("streamtoarray")
 const getPrefixedKeys = async (elDb, prefix = 0) => {
   if (!elDb) throw new Error("Missing args.")
 
-  const gt = prefix ? [prefix, ""].join(":") : "_"
+  const gt = prefix ? [prefix, ""].join(":") : "_\ufff0"
   const lt = prefix ? [prefix, "\ufff0"].join(":") : "\ufff0"
 
   const len = prefix && prefix.length + 1
