7ee8ee2f9dd67babb93a5c3a8a392664067a8960
Robin Millette <robin@millette.info>
2020-03-02 12:47:34 -0500

    Allow empty values (page=).

--- get-prefixed-keys.js
+++ get-prefixed-keys.js
@@ -6,12 +6,16 @@ const { toArray } = require("streamtoarray")
 const getPrefixedKeys = async (elDb, prefix = 0) => {
   if (!elDb) throw new Error("Missing args.")
 
-  const gt = prefix ? [prefix, ""].join(":") : "_\ufff0"
+  const gte = prefix ? [prefix, ""].join(":") : "_\ufff0"
   const lt = prefix ? [prefix, "\ufff0"].join(":") : "\ufff0"
 
   const len = prefix && prefix.length + 1
   const slicer = (id) => id.slice(len)
-  const str = await elDb.createKeyStream({ gt, lt })
+
+  // const str = await elDb.createReadStream({ gte, lt })
+  // return toArray(str)
+
+  const str = await elDb.createKeyStream({ gte, lt })
   const arr = await toArray(str)
   return arr.map(slicer)
 }
