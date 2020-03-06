9364f1bff1919bc8b2187bfa4b1efabeff59dd81
Robin Millette <robin@millette.info>
2020-03-06 15:16:05 -0500

    Update tests.

--- test.js
+++ test.js
@@ -1,6 +1,51 @@
-const wete = require("./index")
+"use strict"
 
-test("wete", () => {
-  const result = wete()
-  expect(result).toBeTruthy()
+const fofo = require("./fofo")
+
+test.skip("create page", async () => {
+  const doc = {
+    editor: "bob",
+    pathname: "",
+    contents: "<p>Hello world",
+  }
+
+  const ddd = await fofo.updatePage(doc)
+  const {
+    data: { creator, createdAt, updatedAt },
+    pathname,
+    contents,
+  } = ddd
+
+  // console.log("DDD", ddd)
+  expect(contents).toBe("<p>Hello world</p>")
+  expect(creator).toBe(doc.editor)
+  expect(createdAt).toBeTruthy()
+  expect(createdAt).toBe(updatedAt)
+  expect(pathname).toBe("/")
+})
+
+test("update page", async () => {
+  const oldDoc = await fofo.updatePage({
+    updatedAt: 1583447300000,
+    editor: "bob",
+    pathname: "/",
+    contents: "<p>Hello world",
+  })
+
+  const {
+    data: { creator, createdAt, updatedAt },
+    contents,
+  } = await fofo.updatePage(
+    {
+      contents: "<p>Hello <i>world",
+      editor: "joe",
+      pathname: "",
+    },
+    oldDoc
+  )
+
+  expect(contents).toBe("<p>Hello <i>world</i></p>")
+  expect(creator).toBe(oldDoc.data.editor)
+  expect(createdAt).toBe(oldDoc.data.updatedAt)
+  expect(updatedAt > createdAt).toBeTruthy()
 })
