264f0337392cb7637413025a72880ef1f82e0980
Robin Millette <robin@millette.info>
2020-04-02 14:01:37 -0400

    Initialize default config (env vars).

--- init.js
+++ init.js
@@ -1,16 +1,54 @@
 "use strict"
 
+// core
+const fs = require("fs").promises
+
+// npm
+const { nanoid } = require("nanoid")
 const freshTemplate = require("./init-template")
 const freshCss = require("./init-css")
 const freshJs = require("./init-js")
+const des = require("dotenv-safe") // .config()
+
+const config = async () => {
+  try {
+    console.log("config...")
+    des.config()
+    console.log("config is ok")
+  } catch (e) {
+    console.error("MISSING:", e.missing)
+    e.missing.forEach((v) => {
+      switch (v) {
+        case "DB":
+          process.env.DB = "db-web-v3.0.0"
+          break
+
+        case "SECRET":
+          process.env.SECRET = nanoid(32)
+          break
+
+        default:
+          throw e
+      }
+    })
+
+    await fs.writeFile(
+      ".env",
+      `DB=${process.env.DB}
+SECRET=${process.env.SECRET}
+`,
+      "utf-8"
+    )
+    console.log("config written")
+  }
+}
 
 const init = async () => {
+  await config()
   console.log("Fresh js...")
   await freshJs()
   console.log("Fresh template and css...")
   return Promise.all([freshTemplate(), freshCss()])
-  // console.log("Launching...")
-  // return dbname
 }
 
 module.exports = init
